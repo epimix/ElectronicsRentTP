d9c4eb2be7f6c019feb1c151ec7b2b686e22817d
epimix <epimiks@gmail.com>
2025-10-24 18:15:24 +0300

    Add project files.

---
+++ ElectronicsRentTP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElectronicsRentTP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
