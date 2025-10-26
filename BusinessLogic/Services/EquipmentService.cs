1a6fd27531658c594327bd1804418d96b57c8376
Andry958 <ad1379125@gmail.com>
2025-10-26 12:55:54 +0200

    Доданий проект BusinessLogic з сервісом для обладнання. Доданий загальний репозиторій

---
+++ BusinessLogic/Services/EquipmentService.cs
@@ -0,0 +1,101 @@
+﻿using BusinessLogic.Interfaces;
+using DataAccess.Data.Entities;
+using DataAccess.Repositories;
+using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class EquipmentService : IEquipmentService
+    {
+        private readonly IRepository<Equipment> repo;
+
+        public EquipmentService(IRepository<Equipment> repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IList<Equipment>> GetAll(
+            int? filterCategoryId,
+            string? ByName,
+            string? ByDescription,
+            decimal? filterMin,
+            decimal? filterMax,
+            bool? SortPriceAsc,
+            bool? IsAvailable,
+            int pageNumber = 1)
+        {
+            if (pageNumber < 1)
+                pageNumber = 1;
+
+            var filterEx = PredicateBuilder.New<Equipment>(true);
+
+            if (filterCategoryId != null)
+                filterEx = filterEx.And(x => x.CategoryId == filterCategoryId);
+
+            if (!string.IsNullOrWhiteSpace(ByName))
+                filterEx = filterEx.And(x => x.Name.ToLower().Contains(ByName.ToLower()));
+
+            if (!string.IsNullOrWhiteSpace(ByDescription))
+                filterEx = filterEx.And(x => x.Description.ToLower().Contains(ByDescription.ToLower()));
+
+            if (filterMin != null)
+                filterEx = filterEx.And(x => x.PricePerHour >= filterMin.Value);
+
+            if (filterMax != null)
+                filterEx = filterEx.And(x => x.PricePerHour <= filterMax.Value);
+
+            if (IsAvailable != null)
+                filterEx = filterEx.And(x => x.IsAvailable == IsAvailable.Value);
+
+            var items = await repo.GetAllAsync(filtering: filterEx, includes: nameof(Equipment.Category));
+
+            if (SortPriceAsc != null)
+            {
+                items = SortPriceAsc.Value
+                    ? items.OrderBy(x => x.PricePerHour).ToList()
+                    : items.OrderByDescending(x => x.PricePerHour).ToList();
+            }
+
+            int pageSize = 5;
+            var res = items.Skip((pageNumber - 1) * pageSize)
+                         .Take(pageSize)
+                         .ToList();
+
+            return res;
+        }
+
+        public async Task AddEquipment(Equipment equipment)
+        {
+            if (equipment == null)
+                return;
+
+            await repo.AddAsync(equipment);
+        }
+
+        public async Task DeleteEquipment(int id)
+        {
+            var equipment = await repo.GetByIdAsync(id);
+            if (equipment == null)
+                return;
+
+            await repo.DeleteAsync(equipment);
+        }
+
+        public async Task<Equipment?> GetById(int id)
+        {
+            return await repo.GetByIdAsync(id);
+        }
+
+        public async Task UpdateEquipment(Equipment equipment)
+        {
+            if (equipment == null)
+                return;
+
+            await repo.UpdateAsync(equipment);
+        }
+    }
+}
---
+++ DataAccess/Repositories/Repository.cs
@@ -0,0 +1,83 @@
+﻿using DataAccess.Data;
+using DataAccess.Data.Entities;
+using DataAccess.Helpers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class, BaseEntity
+    {
+        internal EquipmentRentalDbContext context;
+        internal DbSet<T> set;
+
+        public Repository(EquipmentRentalDbContext context)
+        {
+            this.context = context;
+            this.set = context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await set.AddAsync(entity);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            await DeleteAsync(entity);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            if (entity != null)
+            {
+                set.Remove(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+        public async Task ClearAsync()
+        {
+            var set = context.Set<T>();
+            if (set.Any())
+            {
+                context.RemoveRange(set);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(int? pageNumber = 1, int pageSize = 5, Expression<Func<T, bool>>? filtering = null, params string[]? includes)
+        {
+            var query = set.AsQueryable();
+
+            if (pageNumber != null)
+                query = await query.PaginateAsync(pageNumber.Value, pageSize);
+
+            if (filtering != null)
+                query = query.Where(filtering);
+
+            if (includes != null && includes.Length > 0)
+                foreach (var prop in includes)
+                    query = query.Include(prop);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await set.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+    }
+}
