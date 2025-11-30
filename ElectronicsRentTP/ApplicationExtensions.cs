feaa589d14bdc7b8ae456de3cdb7c2d964a564fd
epimix <epimiks@gmail.com>
2025-11-30 16:41:31 +0200

    host updating

--- ElectronicsRentTP/ApplicationExtensions.cs
+++ ElectronicsRentTP/ApplicationExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Data.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 
 namespace ElectronicsRentTP.Helpers
 {
@@ -11,9 +12,20 @@ namespace ElectronicsRentTP.Helpers
             {
                 var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
                 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 
-                IdentityInitializer.SeedRolesAsync(roleManager).Wait();
-                IdentityInitializer.SeedAdminAsync(userManager).Wait();
+                try
+                {
+                    IdentityInitializer.SeedRolesAsync(roleManager).Wait();
+                    IdentityInitializer.SeedAdminAsync(userManager).Wait();
+                    logger.LogInformation("Roles and admin user seeded successfully.");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Failed to seed roles and admin user. This might be due to database connection issues.");
+                    // Don't throw - allow the app to start even if seeding fails
+                    // The database might not be ready yet or connection string might be wrong
+                }
             }
         }
     }
